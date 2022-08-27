2724174a0d390a610f3f20d8d767b2fbaf6436f9
musicboy17 <pratham@yandex.ru>
2022-08-27 23:14:53 +0300

    EX_002_second step

--- EX_002/Program.cs
+++ EX_002/Program.cs
@@ -3,3 +3,42 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите переменные для первой прямой: ");
+Console.Write("b1 = ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("k1 = ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите переменные для второй прямой: ");
+Console.Write("b2 = ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("k2 = ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = 0;
+
+double y = 0;
+
+
+if ((k1 == k2) && (b1 == b2))
+Console.WriteLine("Прямые совпадают!");
+else if (k1==k2)
+Console.WriteLine("Прямые параллельны!");
+
+else
+
+{
+
+x=(b2-b1)/(k1-k2);
+
+y=(k1*(b2-b1))/(k1-k2)+b1;
+
+Console.WriteLine($"x= {x}, y= {y}");
+
+}
+
+
+
